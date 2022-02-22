62a7ea7b1aac20dfa6c8bdb462283dcf08358806
Adam Sohn <adamsohn@Adams-MacBook-Pro.local>
2022-02-22 11:25:53 -0400

    Remove React dependicies from boilerplate

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -1,9 +1,1 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './App'
-
-console.log('popup script')
-
-const root = document.querySelector('#root')
-
-render(<App />, root)
+console.log('popup script');

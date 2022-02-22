234a0d6da6f914b572589606776ab00c56af5b07
Adam Sohn <adamsohn@Adams-MacBook-Pro.local>
2022-02-22 18:39:40 -0400

    Refactored and cleaned service worker code

--- src/serviceworker.ts
+++ src/serviceworker.ts
@@ -1,51 +1,54 @@
 type Result = { id: number; containsString: boolean };
 
+const notEmpty = <TValue>(
+  value: TValue | null | undefined,
+): value is TValue => {
+  if (value === null || value === undefined) return false;
+  return true;
+};
+
 const setTitle = (text: string) => {
   if (!document.title || typeof text !== 'string') return false;
   return document.title.toLowerCase().includes(text.toLowerCase());
 };
+
 chrome.runtime.onMessage.addListener((event) => {
   if (event.type === 'enterpress') {
     const text = event.payload;
     chrome.tabs.query({}, async (tabs) => {
-      let count = tabs.length;
-      const values = new Promise<Result[]>((resolve) => {
-        const results: Result[] = [];
-        tabs.forEach(({ id }) => {
-          if (id) {
+      const values = await Promise.all(
+        tabs.map(async ({ id }) => {
+          if (!id) return null;
+          try {
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             //@ts-ignore
-            chrome.scripting.executeScript(
-              {
-                target: { tabId: id },
-                func: setTitle,
-                args: [text],
-              },
-              (value: any) => {
-                if (!chrome.runtime.lastError?.message) {
-                  if (value && value[0])
-                    results.push({ id, containsString: value[0].result });
-                  count--;
-                  if (count === 0) {
-                    resolve(results);
-                  }
-                } else {
-                  console.warn(chrome.runtime.lastError.message);
-                }
-              },
-            );
+            const value = await chrome.scripting.executeScript({
+              target: { tabId: id },
+              func: setTitle,
+              args: [text],
+            });
+            if (value?.[0]?.result) {
+              return { id, containsString: value[0].result } as Result;
+            } else {
+              return null;
+            }
+          } catch (error) {
+            return null;
           }
-        });
-      });
-      // First get the values
-      // Then iterate through them and set the active tab to that id
-      const tab = (await values).find(
-        ({ containsString }: { containsString: boolean }) => containsString,
+        }),
       );
-      console.log(chrome.runtime.lastError);
+
+      const tab = values
+        .filter(notEmpty)
+        .map((value) => {
+          return value;
+        })
+        .find(
+          ({ containsString }: { containsString: boolean }) => containsString,
+        );
+
       const tabId = tab?.id;
       if (tabId) {
-        console.log(chrome.runtime.lastError);
         chrome.tabs.update(tabId, { active: true });
       }
     });
